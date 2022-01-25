6d11fa18f3a20d8287f004e53d2c0df843ac6d6c
David Yanccehuallpa <david.yanccehuallpa@gestionysistemas.com>
2022-01-25 01:13:46 -0500

    fix: error de compilacion

--- code/prediction_alert/test_predictions.py
+++ code/prediction_alert/test_predictions.py
@@ -2,11 +2,10 @@ from notify import notify_hot_match
 from factory import MatchMock
 
 match_mock = MatchMock()
-## mock_shots = match_mock.create("by shots")
-a = match_mock.create("by atttacks")
+
 
 def test_attacks_notification ():
-    assert notify_hot_match(a), "Deberia de alertar, ya que el numero de ataques es superior a los minutos transcurridos"
-    assert notify_hot_match(a), "Deberia notificar"
+    assert notify_hot_match(match_mock.create("by attacks")), "Deberia de alertar, ya que el numero de ataques es superior a los minutos transcurridos"
+    assert notify_hot_match(match_mock.create("by shots")), "Deberia notificar"
     
 
