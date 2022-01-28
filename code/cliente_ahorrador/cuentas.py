2f0eeef5952f043bc36ecb2077fabf0b45f78e63
David Yanccehuallpa <david.yanccehuallpa@gestionysistemas.com>
2022-01-27 20:37:50 -0500

    refactor: abstraccion del problema en clases

--- code/cliente_ahorrador/cuentas.py
+++ code/cliente_ahorrador/cuentas.py
@@ -1,11 +1,13 @@
-from pytest import Instance
+from constante import Constante
 
 
 def monto_valido_cuenta(monto_total, tipo_cambio):
     if not isinstance(monto_total, int):
         return False
-    return monto_total < 1000 * tipo_cambio
+    return monto_total <= Constante.MONTO_MAXIMO_DOLARES_SCOTIABANK * tipo_cambio
 
 def es_deposito_valido_scotiabank(monto_deposito):
-    return monto_deposito % 50 == 0
-    
+    return monto_deposito % Constante.UNIDAD_DEPOSITO_SOLES_SCOTIABANK == 0
+
+def es_deposito_valido_interbank(monto_deposito):
+    return monto_deposito % Constante.UNIDAD_DEPOSITO_DOLARES_INTERBANK == 0
--- code/cliente_ahorrador/test_cliente_ahorrador.py
+++ code/cliente_ahorrador/test_cliente_ahorrador.py
@@ -1,8 +1,13 @@
-from cuentas import monto_valido_cuenta
-from cuentas import es_deposito_valido_scotiabank
+from cuenta import Cuenta
+from cuentas import (
+    monto_valido_cuenta,
+    es_deposito_valido_scotiabank,
+    es_deposito_valido_interbank
+)
 
 def validar_monto_menor_1000_dolares_scotiabank():
-    assert monto_valido_cuenta(1500, 4), "No debe de superar los 1000 dolares"
+    cuenta_scotiabank = Cuenta('1001', 'scotiabank', 1500, 'PEN')
+    assert cuenta_scotiabank.es_valida(4), "No debe de superar los 1000 dolares"
 
 def validar_monto_mayor_1000_dolares_scotiabank():
     assert not monto_valido_cuenta(4100, 4), "El monto esta superando los 1000 dolares"
@@ -23,3 +28,6 @@ def test_cuenta_valida_scotiabank():
 
 def test_deposito_no_valido_scotiabank():
     assert not es_deposito_valido_scotiabank(132), "Debe de ser multiplo de 50"
+
+def test_deposito_no_valido_interbank():
+    assert not es_deposito_valido_interbank(225), "El deposito debe de ser en miles de dolares"
